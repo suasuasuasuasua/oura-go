9b3b16ba263358a6b533d73ce3c76547ec9e7df9
Justin Hoang <j124.dev@proton.me>
2025-09-12 13:03:40 -0600

    feat: setup basic skeleton

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks = {
+    # https://github.com/cachix/git-hooks.nix?tab=readme-ov-file#golang
+    gofmt.enable = true;
+    golangci-lint.enable = true;
+    golines.enable = true;
+    govet.enable = true;
+    staticcheck.enable = true;
+    # https://github.com/cachix/git-hooks.nix?tab=readme-ov-file#nix-1
+    nixfmt-rfc-style.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
