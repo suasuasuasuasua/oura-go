f56539b91ef97bbbbc29aea044a3b56b86d7df02
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2025-09-11 20:22:16 -0600

    chore(devenv): remove other unnecessary code from devenv
    Apply suggestion from @suasuasuasuasua

--- devenv.nix
+++ devenv.nix
@@ -7,31 +7,6 @@
   # https://devenv.sh/languages/
   languages.go.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  enterShell = ''
-    echo
-    echo 🦄 Welcome to the Go development environment!
-    echo
-    go version
-    echo
-  '';
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    go test ./...
-  '';
-
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
